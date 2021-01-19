00e4e4e1e4af0720451f06ba2101d8fd5784c4de
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2021-01-19 11:19:23 -0600

    finalTMP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react'
+import './globals.css';
+import Header from "./header";
+import Overview from './overview';
+import TopCardList from "./top-card-list";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+    <Header />
+    <TopCardList />
+    <Overview />
+  </>
+  )
 }
 
 export default App;
